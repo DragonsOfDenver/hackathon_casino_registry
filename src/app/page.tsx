ce05b6b26055a6beef7b1c57610248cb0793fb8b
dadlessnsad <tbodnar85@gmail.com>
2024-02-26 09:38:21 -0700

    add toast notifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,117 +6,119 @@ import { mainnet } from 'viem/chains'
 import { namehash, normalize } from 'viem/ens'
 import { Form, Field, ErrorMessage, Formik } from 'formik';
 import * as Yup from "yup";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const validationSchema = Yup.object().shape({
-  userInput: Yup.string().required("Required").matches(
-    /^(0x[a-fA-F0-9]{40})|([a-zA-Z0-9-]+\.eth)$/g,
-    "Invalid Ethereum address or ENS name"
-  ),
+    userInput: Yup.string().required("Required").matches(
+        /^(0x[a-fA-F0-9]{40})|([a-zA-Z0-9-]+\.eth)$/g,
+        "Invalid Ethereum address or ENS name"
+    ),
 });
 
 export default function Home() {
-  const [address, setAddress] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<any>(null);
+    const [address, setAddress] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<any>(null);
 
-  const apiURL = "http://localhost:8080/api/storeAddress"
+    const apiURL = process.env.NEXT_PUBLIC_API_URL || '';
+    console.log("API URL: ", apiURL)
 
-  // check for ens names bruh
-  const publicClient = createPublicClient({
-    chain: mainnet,
-    transport: http()
-  })
+    // check for ens names bruh
+    const publicClient = createPublicClient({
+        chain: mainnet,
+        transport: http()
+    })
 
-  const CheckAddressToENS = async () => {
-    setLoading(true);
-    setError(null);
+    const CheckAddressToENS = async (currentAddress: string) => {
+        setLoading(true);
+        setError(null);
+        try {
+            // Check if the input is an ENS name or an address
+            // by checking if it has a dot eth at the end 
+            if (currentAddress.endsWith(".eth")) {
+                const ResolverAddress = await publicClient.getEnsAddress({
+                    name: normalize(currentAddress),
+                })
 
-    // Check if the input is an ENS name or an address
-    // by checking if it has a dot eth at the end 
-    if (address.endsWith(".eth")) {
-      const ResolverAddress = await publicClient.getEnsAddress({
-        name: normalize(address),
-      })
+                if (ResolverAddress === null) {
+                    setError("No Address found for the ENS name")
+                    toast.error("No Address found for the ENS name")
+                } else {
 
-      if (ResolverAddress === null) {
-        setError("No Address found for the ENS name")
-      } else {
-
-        const payload = {
-          ResolverAddress: ResolverAddress,
-          address: address
+                    const payload = {
+                        ResolverAddress: ResolverAddress,
+                        address: currentAddress
+                    }
+                    const response = await fetch(apiURL, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(payload)
+                    })
+                }
+                setLoading(false)
+            } else {
+                const payload = {
+                    ResolverAddress: currentAddress,
+                    address: null
+                }
+                const response = await fetch(apiURL, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(payload)
+                })
+            }
+        } catch (error) {
+            setError("Failed to store the address in the database.")
+            toast.error("Failed to store the address in the database.")
+            setLoading(false)
+        } finally {
+            setLoading(false);
+            toast.success("We got your address :)")
         }
-        const response = await fetch(apiURL, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(payload)
-        })
-
-        if (!response.ok) {
-          setError("Failed to store the address and the ENS name in the database.")
-        }
-        setError(null)
-      }
-      setLoading(false)
-    } else {
-      const payload = {
-        ResolverAddress: null,
-        address: address
-      }
-      const response = await fetch(apiURL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
-      })
-
-      if (!response.ok) {
-        setError("Failed to store the address and the ENS name in the database.")
-      }
-      setError(null)
-      setLoading(false)
     }
-  }
 
-  return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Wanna join the Bufficorn Casino?</h1>
-        <p className={styles.description}>
-          Enter your address or ENS name to join the Bufficorn Casino, come time to play.
-        </p>
+    return (
+        <main className={styles.main}>
+            <div className={styles.container}>
+                <h1 className={styles.title}>Wanna join the Bufficorn Casino?</h1>
+                <p className={styles.description}>
+                    Enter your address or ENS name to join the Bufficorn Casino, come time to play.
+                </p>
 
-        <Formik
-          initialValues={{ userInput: "" }}
-          validationSchema={validationSchema}
-          onSubmit={(values, { setSubmitting }) => {
-            setError(null); // Clear previous errors
-            setAddress(values.userInput); // Set address
-            CheckAddressToENS(); // Check address or ENS name
-            setSubmitting(false); // Finish form submission
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form className={styles.form}>
-              <Field
-                type="text"
-                name="userInput"
-                placeholder="Enter here 🦄🎰🎲🃏🎮🎰🦄"
-                className={styles.input}
-              />
-              <ErrorMessage name="userInput" component="div" className={styles.error} />
-              <button type="submit" className={styles.button} disabled={isSubmitting || loading || error }>
-                Join Bufficorn Casino
-              </button>
-            </Form>
-          )}
-        </Formik>
+                <Formik
+                    initialValues={{ userInput: "" }}
+                    validationSchema={validationSchema}
+                    onSubmit={(values, { setSubmitting }) => {
+                        setError(null); // Clear previous errors
+                        setAddress(values.userInput); // Set address
+                        CheckAddressToENS(values.userInput); // Check address
+                        setSubmitting(false); // Finish form submission
+                    }}
+                >
+                    {({ isSubmitting }) => (
+                        <Form className={styles.form}>
+                            <Field
+                                type="text"
+                                name="userInput"
+                                placeholder="Enter here 🦄🎰🎲🃏🎮🎰🦄"
+                                className={styles.input}
+                            />
+                            <ErrorMessage name="userInput" component="div" className={styles.error} />
+                            <button type="submit" className={styles.button} disabled={isSubmitting || loading || error}>
+                                Join Bufficorn Casino
+                            </button>
+                        </Form>
+                    )}
+                </Formik>
 
-      </div>
-    </main>
-  );
+            </div>
+            <ToastContainer position="bottom-center" autoClose={5000} />
+        </main>
+    );
 }
